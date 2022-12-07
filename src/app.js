4db8f3c6317b1fcdb63edb3b7f0a6175c8034ba3
LaidyYesi <cantamosaleluya@gmail.com>
2022-12-07 08:24:25 -0300

    Agregado de carpetas y archivos sprint2

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// Modulo nativo para manejar las rutas de los archivos
+const path = require("path");
+
+// Usando recursos estáticos.
+app.use(express.static("public"));
+
+
+// Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/home.html"))
+})
+
+// Ponemos a escuchar el servidor
+app.listen(3030, () => {
+    console.log("Servidor corriendo en http://localhost:3030")
+});
