f59b30a0745aa726256bbb1a30124baf64e5a7d4
gbogom <gbogom@gmail.com>
2023-02-02 18:55:06 -0500

    modifcar navbar cuando hay session

--- src/middlewares/logUserMiddleware.js
+++ src/middlewares/logUserMiddleware.js
@@ -1,5 +1,10 @@
 function logUserMiddleware(req, res, next) {
     res.locals.isLog = false;
+
+    if (req.session.userLogueado) {
+		res.locals.isLog = true;
+		res.locals.userLogueado = req.session.userLogueado;
+	}
     
     next();
 
