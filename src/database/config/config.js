d2fac773a03017791b3ec85d6b9336271a811288
MartinFaraguna <faraguna.martin@gmail.com>
2023-04-20 22:32:28 -0300

    arreglos css

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "",
-    "database": "petstore",
+    "database": "petmarket",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
