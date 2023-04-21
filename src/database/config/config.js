cc36761f13a61a25c20893b1ad52a2ecfedc4ffb
canorena3d <canorena3d@gmail.com>
2023-04-20 20:25:47 -0500

    Solucion, Error eliminar producto y Error color de fondo input price y discount para crear y editar productos.

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
-    "database": "petstore",
+    "password": "Animacion3d*",
+    "database": "petmarket",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
