3e89fb1b163712cefe0b3fec13e1aab4aafa77e3
leidyyesi <cantamosaleluya@gmail.com>
2023-04-27 20:44:21 -0300

    ultimas pruebas

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "Animacion3d*",
+    "password": "",
     "database": "petmarket",
     "host": "127.0.0.1",
     "dialect": "mysql"
